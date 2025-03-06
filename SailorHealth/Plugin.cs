2fbad7fe858a4c2a7f9f96ad6f6b30e521e4ee40
Bryon Bailey <bryon.r.bailey@gmail.com>
2025-03-05 19:31:24 -0500

    Add project files.

---
+++ SailorHealth/Plugin.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
+
+namespace SailorHealth
+{
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string PLUGIN_GUID = "com.raddude82.sailorhealth";
+        public const string PLUGIN_NAME = "SailorHealth";
+        public const string PLUGIN_VERSION = "1.1.0";
+
+        internal static Plugin instance;
+        internal static ManualLogSource logger;
+
+        private void Awake()
+        {
+            instance = this;
+            logger = Logger;
+
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), PLUGIN_GUID);
+        }
+    }
+}
+
